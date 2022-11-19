51957b831175df9cfc4e79ab0940b899e9f0ce82
eobcre <eobcre@gmail.com>
2022-11-18 19:53:11 -0500

    Rotate function keep moving to right

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const themeContainer = document.querySelector('.theme-container');
+
 document.querySelector('.toggle-button').addEventListener('click', () => {
   document.body.classList.toggle('dark');
+  const currentRotation = parseInt(
+    getComputedStyle(themeContainer).getPropertyValue('--rotation')
+  );
+  themeContainer.style.setProperty('--rotation', currentRotation + 180);
 });
